bab4370e7e4b300fa35b809ad0d3e1807bdda0f2
rahul <rahul@rahul-PC>
2019-09-27 11:31:19 +0530

    task 17 completed

---
+++ src/main/java/com/rahul/Util.java
@@ -0,0 +1,15 @@
+package com.rahul;
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Util {
+
+	public static SessionFactory getSF(){
+		Configuration config=new Configuration().configure();
+		SessionFactory sf=config.buildSessionFactory();
+		return sf;
+	}
+
+}
